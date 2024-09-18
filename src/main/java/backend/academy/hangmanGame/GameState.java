068d6a523a527e93531481d1566832448893eee2
cr1stup <scaleupgo@gmail.com>
2024-09-18 10:19:29 +0300

    feat(GameState): add a guessing check

--- src/main/java/backend/academy/hangmanGame/GameState.java
+++ src/main/java/backend/academy/hangmanGame/GameState.java
@@ -1,5 +1,7 @@
 package backend.academy.hangmanGame;
 
+import java.util.HashSet;
+import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,14 +11,19 @@ public class GameState {
     private String hiddenWord;
     private String hiddenViewWord;
     private String hint;
+    private HangmanPicture hangmanPicture;
     private int category;
     private int complexity;
     private int maxAttempt;
+    private int errorCount;
+    private int numOfGuessedLetters;
     private String[] complexityPattern;
+    private final Set<Character> errorChar = new HashSet<>();
     private final WordSelector wordSelector = new WordSelector();
     private final ComplexitySelector complexitySelector = new ComplexitySelector();
 
     public GameState() {
+        hangmanPicture = HangmanPicture.ZERO;
         hiddenWord = "";
         hiddenViewWord = "";
         hint = "";
@@ -25,6 +32,23 @@ public class GameState {
     }
 
     public void checkGuess(char guess) {
+        if (hiddenWord.contains(String.valueOf(guess))) {
+            char[] tempView = hiddenViewWord.toCharArray();
+
+            for (int i = 0; i < hiddenWord.length(); i++) {
+                if (hiddenWord.charAt(i) == guess) {
+                    numOfGuessedLetters++;
+                    tempView[i] = guess;
+                }
+            }
+
+            hiddenViewWord = new String(tempView);
+
+        } else {
+            errorChar.add(guess);
+            errorCount++;
+            hangmanPicture = HangmanPicture.valueOf(complexityPattern[errorCount]);
+        }
     }
 
     public void settingHiddenWord(int category) {
