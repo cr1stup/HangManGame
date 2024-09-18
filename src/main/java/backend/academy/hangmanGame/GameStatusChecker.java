687a61e65b457b7502c97c6c20eedc0abe65aae4
cr1stup <scaleupgo@gmail.com>
2024-09-18 15:05:13 +0300

    feat: add game status check

--- src/main/java/backend/academy/hangmanGame/GameStatusChecker.java
+++ src/main/java/backend/academy/hangmanGame/GameStatusChecker.java
@@ -4,7 +4,15 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class GameStatusChecker {
-    public static GameStatus check() {
+    public static GameStatus check(GameState game) {
+        if (game.hiddenWord() == null || game.hiddenWord().isBlank()) {
+            return GameStatus.ERROR;
+        } else if (game.getRemainingAttempts() == 0) {
+            return GameStatus.LOSS;
+        } else if (game.numOfGuessedLetters() == game.hiddenViewWord().length()) {
+            return GameStatus.WIN;
+        }
+
         return GameStatus.ONGOING;
     }
 }
