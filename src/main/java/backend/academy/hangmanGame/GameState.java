7d465dc8753c036d582b00e6794b0ed8f136e760
cr1stup <scaleupgo@gmail.com>
2024-09-18 09:43:41 +0300

    feat(GameState): add setting complexity

--- src/main/java/backend/academy/hangmanGame/GameState.java
+++ src/main/java/backend/academy/hangmanGame/GameState.java
@@ -10,13 +10,18 @@ public class GameState {
     private String hiddenViewWord;
     private String hint;
     private int category;
+    private int complexity;
+    private int maxAttempt;
+    private String[] complexityPattern;
     private final WordSelector wordSelector = new WordSelector();
+    private final ComplexitySelector complexitySelector = new ComplexitySelector();
 
     public GameState() {
         hiddenWord = "";
         hiddenViewWord = "";
         hint = "";
         category = 1;
+        complexity = 1;
     }
 
     public void checkGuess(char guess) {
@@ -27,5 +32,11 @@ public class GameState {
         hint = wordSelector.hint();
         hiddenViewWord = "_".repeat(hiddenWord.length());
         this.category = category;
+    }
+
+    public void settingComplexity(int complexity) {
+        complexityPattern = complexitySelector.getComplexityPattern(complexity);
+        maxAttempt = complexitySelector.getMaxError(complexity);
+        this.complexity = complexity;
     }
 }
