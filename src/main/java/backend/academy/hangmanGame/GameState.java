8927c37d282cd244fa667771947936a2507cfdbd
cr1stup <scaleupgo@gmail.com>
2024-09-18 09:36:46 +0300

    feat(GameState): add setting hidden word

--- src/main/java/backend/academy/hangmanGame/GameState.java
+++ src/main/java/backend/academy/hangmanGame/GameState.java
@@ -1,11 +1,31 @@
 package backend.academy.hangmanGame;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class GameState {
+    private String hiddenWord;
+    private String hiddenViewWord;
+    private String hint;
+    private int category;
+    private final WordSelector wordSelector = new WordSelector();
 
     public GameState() {
+        hiddenWord = "";
+        hiddenViewWord = "";
+        hint = "";
+        category = 1;
     }
 
     public void checkGuess(char guess) {
+    }
 
+    public void settingHiddenWord(int category) {
+        hiddenWord = wordSelector.getRandomWord(category);
+        hint = wordSelector.hint();
+        hiddenViewWord = "_".repeat(hiddenWord.length());
+        this.category = category;
     }
 }
