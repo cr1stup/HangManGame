e65d32504562ca6b5bdf16e1041cc68fb427d95d
cr1stup <scaleupgo@gmail.com>
2024-09-18 15:11:12 +0300

    feat(Render): add render end of the game

--- src/main/java/backend/academy/hangmanGame/GameRenderer.java
+++ src/main/java/backend/academy/hangmanGame/GameRenderer.java
@@ -2,6 +2,7 @@ package backend.academy.hangmanGame;
 
 import java.io.PrintStream;
 
+@SuppressWarnings("MultipleStringLiterals")
 public class GameRenderer {
     private final PrintStream printStream;
 
@@ -14,6 +15,16 @@ public class GameRenderer {
         printStream.println("Hidden word: " + game.hiddenViewWord());
         printStream.println("Remaining attempts: " + game.getRemainingAttempts());
         printStream.println("To use hint type: \"hint\"");
+        printStream.println();
+    }
+
+    public void showEndOfGame(GameStatus status, GameState game) {
+        if (status == GameStatus.WIN) {
+            printStream.println("You win!");
+        } else {
+            printStream.println("You lose!");
+            printStream.println("Hidden word: " + game.hiddenWord());
+        }
         printStream.println();
     }
 }
