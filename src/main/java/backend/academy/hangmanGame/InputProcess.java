94cd63a8c6d5102dc83bdd61449e466b02b2d14f
cr1stup <scaleupgo@gmail.com>
2024-09-18 10:01:29 +0300

    feat(Input): add complexity input

--- src/main/java/backend/academy/hangmanGame/InputProcess.java
+++ src/main/java/backend/academy/hangmanGame/InputProcess.java
@@ -11,12 +11,16 @@ public class InputProcess {
     private final Scanner scanner;
     private final PrintStream printStream;
     private final SecureRandom random;
+    private final String digits;
+    private final String errorMessage;
 
     public InputProcess(InputStream inputStream, PrintStream printStream) {
         Charset charset = StandardCharsets.UTF_8;
         this.scanner = new Scanner(inputStream, charset);
         this.printStream = printStream;
         random = new SecureRandom();
+        digits = "-?\\d+(\\.\\d+)?";
+        errorMessage = "%nInvalid input! Try again";
     }
 
     public char inputLetter() {
@@ -31,19 +35,44 @@ public class InputProcess {
                 printStream.println(i + ". " + wordSelector.categories()[i]);
             }
 
-            printStream.print("\nEnter category: ");
+            printStream.format("%nEnter category: ");
             String category = scanner.nextLine();
 
             if (category.isBlank()) {
                 printStream.println();
                 return getRandomNum(wordSelector.categories().length);
-            } else if (!category.matches("-?\\d+(\\.\\d+)?")
+            } else if (!category.matches(digits)
                 || Integer.parseInt(category) <= 0
                 || Integer.parseInt(category) >= wordSelector.categories().length) {
-                printStream.println("\nInvalid input! Try again");
+                printStream.format(errorMessage);
             } else {
                 printStream.println();
                 return Integer.parseInt(category);
+            }
+        }
+    }
+
+    public int inputComplexity() {
+        ComplexitySelector complexitySelector = new ComplexitySelector();
+        while (true) {
+            for (int i = 1; i < complexitySelector.complexities().length; i++) {
+                printStream.format("%d. %s (%d attempts)%n",
+                    i, complexitySelector.complexities()[i], complexitySelector.getMaxError(i));
+            }
+
+            printStream.format("%nEnter complexity: ");
+            String complexity = scanner.nextLine();
+
+            if (complexity.isBlank()) {
+                printStream.println();
+                return getRandomNum(complexitySelector.complexities().length);
+            } else if (!complexity.matches(digits)
+                || Integer.parseInt(complexity) <= 0
+                || Integer.parseInt(complexity) >= complexitySelector.complexities().length) {
+                printStream.format(errorMessage);
+            } else {
+                printStream.println();
+                return Integer.parseInt(complexity);
             }
         }
     }
