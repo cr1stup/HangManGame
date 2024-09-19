30671d3b10ae5df5f0e4ec7fe0c805c71d47c102
cr1stup <scaleupgo@gmail.com>
2024-09-19 07:54:58 +0300

    test(TestUtils): redo the functions
    enter parameters from the console and
    add an output reading function

--- src/test/java/backend/academy/hangmanTests/TestUtils.java
+++ src/test/java/backend/academy/hangmanTests/TestUtils.java
@@ -2,29 +2,72 @@ package backend.academy.hangmanTests;
 
 import backend.academy.hangmanGame.GameState;
 import backend.academy.hangmanGame.HangmanPicture;
+import backend.academy.hangmanGame.InputProcess;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class TestUtils {
 
-    private final GameState game = new GameState();
+    private final GameState game;
+    private final PrintStream ps;
+    private final ByteArrayOutputStream byteArrayOutputStream;
 
-    public HangmanPicture getActualHangmanPicture(String hiddenWord, char guess) {
+    public TestUtils() {
+        game = new GameState();
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        ps = new PrintStream(byteArrayOutputStream);
+    }
+
+    public HangmanPicture getActualHangmanPicture(String hiddenWord, String input) {
+        input = input + String.format("%n");
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        InputProcess inputProcess = new InputProcess(inputStream, ps);
+
+        char guess = inputProcess.inputLetter(new GameState());
+
         game.hiddenWord(hiddenWord);
         game.hiddenViewWord("_".repeat(hiddenWord.length()));
         game.checkGuess(guess);
+
         return game.hangmanPicture();
     }
 
-    public String getActualHiddenViewWord(String hiddenWord, char guess) {
+    public String getActualHiddenViewWord(String hiddenWord, String input) {
+        input = input + String.format("%n");
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        InputProcess inputProcess = new InputProcess(inputStream, ps);
+
+        char guess = inputProcess.inputLetter(new GameState());
+
         game.hiddenWord(hiddenWord);
         game.hiddenViewWord("_".repeat(hiddenWord.length()));
         game.checkGuess(guess);
+
         return game.hiddenViewWord();
     }
 
-    public int getActualRemainingAttempts(String hiddenWord, char guess) {
+    public int getActualRemainingAttempts(String hiddenWord, String input) {
+        input = input + String.format("%n");
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        InputProcess inputProcess = new InputProcess(inputStream, ps);
+
+        char guess = inputProcess.inputLetter(new GameState());
+
         game.hiddenWord(hiddenWord);
         game.hiddenViewWord("_".repeat(hiddenWord.length()));
         game.checkGuess(guess);
+
         return game.getRemainingAttempts();
+    }
+
+    public String getOutput(String input) {
+        input = input + String.format("%n");
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+
+        InputProcess inputProcess = new InputProcess(inputStream, ps);
+        inputProcess.inputLetter(new GameState());
+
+        return byteArrayOutputStream.toString();
     }
 }
